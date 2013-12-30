2f6cd1f56e39ac279612a2df3912325f8a53de93
belial <belial@belial.fedoralaptop>
2013-12-29 21:48:03 -0500

    Revert "Added Global CPU Voltage table used for adjusting voltage table"
    This reverts commit 0a4732ade1b9aafdac8a2d5853542ecdd13b7634.

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -376,7 +376,7 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 	.verify		= msm_cpufreq_verify,
 	.target		= msm_cpufreq_target,
 	.name		= "msm",
-	.attr		= msm_cpufreq_attr,
+f	.attr		= msm_cpufreq_attr,
 };
 
 static struct notifier_block msm_cpufreq_pm_notifier = {
